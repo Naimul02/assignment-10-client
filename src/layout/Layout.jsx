a2fe88452c3e8966b8bb143cc389566c5a88dfaa
Naimul02 <naimulislum39@gmail.com>
2024-04-27 20:23:27 +0600

    added footer section

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../component/Navbar/Navbar";
+import Footer from "../component/Footer/Footer";
 
 const Layout = () => {
   return (
-    <div className="max-w-6xl mx-auto">
-      <Navbar></Navbar>
-      <Outlet></Outlet>
+    <div>
+      <div className="max-w-6xl mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
